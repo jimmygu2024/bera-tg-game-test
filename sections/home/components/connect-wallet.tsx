6769cd3fd3a52479825b7545e6942e364fbb31e8
jimmygu <jimmyguu2024@gmail.com>
2024-12-18 18:32:19 +0800

    feat: add wallet connect

---
+++ sections/home/components/connect-wallet.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { TonConnectButton } from '@tonconnect/ui-react';
+
+const ConnectWallet = () => {
+
+  return (
+    <>
+      <TonConnectButton className="whitespace-nowrap" />
+    </>
+  );
+};
+
+export default ConnectWallet;
--- sections/home/index.tsx
+++ sections/home/index.tsx
@@ -8,6 +8,7 @@ import ProgressBar from './components/ProgressBar';
 import { SceneList } from '@/sections/home/components/types';
 import { useRouter } from 'next/navigation'
 import { useAudio } from '@/hooks/useAudio';
+import ConnectWallet from '@/sections/home/components/connect-wallet';
 
 const Home = () => {
 
@@ -82,29 +83,32 @@ const Home = () => {
         />
       </div>
 
-      <div className="flex gap-2 items-center">
-        <button
-          type="button"
-          className="h-[32px] border border-[#4B371F] rounded-[10px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center"
-          onClick={() => {
-            const sceneList = Object.values(SceneList);
-            bgRef.current?.handleNextScene(sceneList[sceneIdx]);
-            let _sceneIdx = sceneIdx + 1;
-            if (_sceneIdx > sceneList.length - 1) {
-              _sceneIdx = 0;
-            }
-            setSceneIdx(_sceneIdx);
-          }}
-        >
-          Next Scene
-        </button>
-        <button
-          onClick={() => router.push('/boost')}
-          type="button"
-          className="h-[32px] border border-[#4B371F] rounded-[10px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center"
-        >
-          Shop Scene
-        </button>
+      <div className="flex gap-2 justify-between p-[10px_10px]">
+        <div className="flex flex-wrap gap-2 items-center">
+          <button
+            type="button"
+            className="h-[32px] border border-[#4B371F] rounded-[10px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center whitespace-nowrap"
+            onClick={() => {
+              const sceneList = Object.values(SceneList);
+              bgRef.current?.handleNextScene(sceneList[sceneIdx]);
+              let _sceneIdx = sceneIdx + 1;
+              if (_sceneIdx > sceneList.length - 1) {
+                _sceneIdx = 0;
+              }
+              setSceneIdx(_sceneIdx);
+            }}
+          >
+            Next Scene
+          </button>
+          <button
+            onClick={() => router.push('/boost')}
+            type="button"
+            className="h-[32px] border border-[#4B371F] rounded-[10px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center whitespace-nowrap"
+          >
+            Shop Scene
+          </button>
+        </div>
+        <ConnectWallet />
       </div>
     </HomeBg>
   );
