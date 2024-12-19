7a3df93404111194272b601f0f61655904a97a6c
jimmygu <jimmyguu2024@gmail.com>
2024-12-19 17:48:49 +0800

    feat: test okx connector 8

--- hooks/useOkxUniversal.ts
+++ hooks/useOkxUniversal.ts
@@ -17,54 +17,58 @@ export function useOkxUniversal(): IOkxUniversalContext {
 
   const onConnect = async () => {
     setConnecting(true);
-    let _session = await okxUniversalProvider?.connect?.({
-      namespaces: {
-        eip155: {
-          chains: ['eip155:1'],
-          defaultChain: '1'
+    let _session;
+    try {
+      _session = await okxUniversalProvider?.connect?.({
+        namespaces: {
+          eip155: {
+            chains: ['eip155:1'],
+            defaultChain: '1'
+          },
         },
-      },
-      optionalNamespaces: {
-        eip155: {
-          chains: [
-            'eip155:8453',
-            'eip155:5000',
-            'eip155:42161',
-            'eip155:43114',
-            'eip155:56',
-            'eip155:59144',
-            'eip155:1088',
-            'eip155:10',
-            'eip155:137',
-            'eip155:1101',
-            'eip155:324',
-            'eip155:100',
-            'eip155:169',
-            'eip155:534352',
-            'eip155:81457',
-            'eip155:34443',
-          ]
+        optionalNamespaces: {
+          eip155: {
+            chains: [
+              'eip155:8453',
+              'eip155:5000',
+              'eip155:42161',
+              'eip155:43114',
+              'eip155:56',
+              'eip155:59144',
+              'eip155:1088',
+              'eip155:10',
+              'eip155:137',
+              'eip155:1101',
+              'eip155:324',
+              'eip155:100',
+              'eip155:169',
+              'eip155:534352',
+              'eip155:81457',
+              'eip155:34443',
+            ]
+          },
+          ton: {
+            chains: [
+              'ton:-239'
+            ],
+            params: {
+              'ton_addr': {
+                'name': 'ton_addr'
+              },
+              // 'ton_proof': {
+              //   'name': 'ton_proof',
+              //   'payload': 'Test'
+              // }
+            }
+          },
         },
-        ton: {
-          chains: [
-            'ton:-239'
-          ],
-          defaultChain: 'ton:-239',
-          params: {
-            'ton_addr': {
-              'name': 'ton_addr'
-            },
-            // 'ton_proof': {
-            //   'name': 'ton_proof',
-            //   'payload': 'Test'
-            // }
-          }
-        },
-      },
-      sessionConfig: {
-        redirect: 'tg://resolve'
-      }
-    });
+        sessionConfig: {
+          redirect: 'tg://resolve'
+        }
+      });
+    } catch (err) {
+      console.log('connect failed: %o', err);
+    }
     setSession(_session);
     setConnecting(false);
     if (_session) {
