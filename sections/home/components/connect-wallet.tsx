cc819afb12f9e6aa2a9a3c93f0e86ae5fef52f75
jimmygu <jimmyguu2024@gmail.com>
2024-12-19 14:51:19 +0800

    feat: test okx connector 2

--- sections/home/components/connect-wallet.tsx
+++ sections/home/components/connect-wallet.tsx
@@ -5,13 +5,21 @@ import Drawer from '@components/Drawer';
 import { useState } from 'react';
 import { useTelegram } from '@/hooks/useTelegram';
 import { UserData } from '@/hooks/useLogin';
+import Skeleton from 'react-loading-skeleton';
 
 const ConnectWallet = () => {
-  const { account, wallet, connected, onConnect, onDisconnect } = useOkxTon();
+  const {
+    account,
+    wallet,
+    connected,
+    onConnect,
+    onDisconnect,
+    connecting
+  } = useOkxTon();
   const { WebApp } = useTelegram();
   const [visible, setVisible] = useState(false);
 
-  const tgUser = WebApp.initDataUnsafe.user as UserData;
+  const tgUser = WebApp?.initDataUnsafe?.user as UserData;
 
   const handleConnect = () => {
     if (connected) {
@@ -29,13 +37,19 @@ const ConnectWallet = () => {
 
   return (
     <>
-      <button
-        type="button"
-        className="h-[32px] border border-[#4B371F] rounded-[10px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center whitespace-nowrap"
-        onClick={handleConnect}
-      >
-        {connected ? `${account?.address?.slice(0, 7)}...${account?.address?.slice(-4)}` : 'Connect'}
-      </button>
+      {
+        connecting ? (
+          <Skeleton width="6.88rem" height="2rem" borderRadius="1rem" />
+        ) : (
+          <button
+            type="button"
+            className="h-[32px] border border-[#4B371F] rounded-[16px] text-[16px] px-[20px] text-black bg-[#C7FF6E] flex justify-center items-center whitespace-nowrap"
+            onClick={handleConnect}
+          >
+            {connected ? `${account?.address?.slice(0, 7)}...${account?.address?.slice(-4)}` : 'Connect'}
+          </button>
+        )
+      }
       <Drawer
         visible={visible}
         onClose={() => {
@@ -46,8 +60,8 @@ const ConnectWallet = () => {
         <div className="p-[20px_10px] h-full flex flex-col items-stretch">
           <div className="flex items-center gap-[10px]">
             {
-              tgUser.photo_url ? (
-                <img src={tgUser.photo_url} alt="" className="shrink-0 h-[28px] w-[28px] rounded-full" />
+              tgUser?.photo_url ? (
+                <img src={tgUser?.photo_url} alt="" className="shrink-0 h-[28px] w-[28px] rounded-full" />
               ) : (
                 <div className="shrink-0 h-[28px] w-[28px] rounded-full border border-[#1F2229] bg-[conic-gradient(from_180deg_at_50%_50%,_#00D1FF_0deg,_#FF008A_360deg)]"></div>
               )
