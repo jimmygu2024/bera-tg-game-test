7edf2c9ebf178100fb80a3744c165cbb132c65aa
jimmygu <jimmyguu2024@gmail.com>
2024-12-30 18:17:22 +0800

    feat: hide import eq when user had been bind

--- sections/home2/components/header.tsx
+++ sections/home2/components/header.tsx
@@ -1,12 +1,19 @@
 import User from '@/sections/home2/components/user';
 import ImportEquipments from '@/sections/home2/components/import-equipments';
+import { useContext } from 'react';
+import { HomeContext } from '@/sections/home2';
 
 const Header = () => {
+  const { userInfo } = useContext(HomeContext);
 
   return (
     <div className="px-[8px] pt-[8px] flex items-center justify-between shrink-0">
       <User />
-      <ImportEquipments />
+      {
+        !!userInfo?.address && (
+          <ImportEquipments />
+        )
+      }
     </div>
   );
 };
