07ecf10344a8da4f57947e44b1a3c333c4e3b644
Yyy <snwyz1st@gmail.com>
2024-12-31 14:33:07 +0800

    fix invite

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -38,7 +38,7 @@ const useLogin = (): UseLoginResult => {
         tg_username: tgUser.username,
         tg_avatar: tgUser.photo_url,
         init_data: WebApp.initData,
-        ...(invite_source && { invite_source: 'okx_invite' }), // only for okx invite
+        invite_source: 'okx_invite', // only for okx invite
         ...(inviterId && Number(inviterId) !== Number(tgUser.id)  &&  { inviter_tg_user_id: inviterId })
       };
       const response = await post('/api/login', loginData);
