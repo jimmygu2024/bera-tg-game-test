238a5749efbb5d8142baf38e602f8efde4eb68b3
jimmygu <jimmyguu2024@gmail.com>
2024-12-31 15:52:23 +0800

    fix: add api env

--- utils/http.ts
+++ utils/http.ts
@@ -26,7 +26,7 @@ export const objectToQueryString = (obj: Record<string, any>): string => {
 
 const AUTH_TOKENS = `_user`;
 
-const BASE_URL = 'https://dev-api-game.beratown.app';
+const BASE_URL = process.env.NEXT_PUBLIC_API || 'https://dev-api-game.beratown.app';
 
 const getUrl = (url: string) => {
   return url.startsWith('http') ? url : `${BASE_URL}${url}`;
