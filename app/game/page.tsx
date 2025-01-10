bc94a270ec88aa804a44a917be92a81a8b6ae805
jimmygu <jimmyguu2024@gmail.com>
2025-01-10 17:51:11 +0800

    feat: test game postMessage 5

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -14,7 +14,7 @@ const Game = () => {
     console.log('WebApp: %o', WebApp);
 
     if (!gameRef.current || !WebApp) return;
-    gameRef.current.contentWindow.postMessage({
+    const data = {
       type: 'Beraciaga',
       data: {
         initData: WebApp.initData,
@@ -29,7 +29,9 @@ const Game = () => {
         APP_LINK: process.env.NEXT_PUBLIC_APP_LINK,
         BERA_IMPORTED_URL: process.env.NEXT_PUBLIC_BERA_IMPORTED_URL,
       },
-    }, '*');
+    };
+    console.log('data will be send: %o', data);
+    gameRef.current.contentWindow.postMessage(JSON.stringify(data), '*');
   };
 
   useEffect(() => {
