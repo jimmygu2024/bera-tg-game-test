7cad49d63d04069d1b035a6d956c76b469140088
jimmygu <jimmyguu2024@gmail.com>
2025-01-10 16:43:07 +0800

    feat: test game postMessage 2

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -24,7 +24,6 @@ const Game = () => {
         API: process.env.NEXT_PUBLIC_API,
         APP_LINK: process.env.NEXT_PUBLIC_APP_LINK,
         BERA_IMPORTED_URL: process.env.NEXT_PUBLIC_BERA_IMPORTED_URL,
-        CRYPTO_KEY: process.env.NEXT_PUBLIC_CRYPTO_KEY,
       },
     }, '*');
   };
@@ -38,7 +37,7 @@ const Game = () => {
       <iframe
         className="w-full h-full"
         ref={gameRef}
-        src="http://localhost:3001"
+        src={process.env.NEXT_PUBLIC_GAME_URL || 'https://bera-tg-game-test.vercel.app/'}
         onLoad={handleLoaded}
       />
     </div>
